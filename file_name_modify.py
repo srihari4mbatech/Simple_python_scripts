8b44a57d268d9e83d6f32c79bb508acd59b248ba
srihari4mbatech <srihari.mba@gmail.com>
2018-04-19 16:56:59 +0530

    I have added couple of new lines and error detection

--- file_name_modify.py
+++ file_name_modify.py
@@ -6,10 +6,20 @@ def change_file_name():
     pass
 
 def read_files(path_dire):
-    list_file=[]
-    for t in os.list(path_dire):
-        list_file.append(t)
-    return list_file
+    if not isinstance(path_dire,str):
+        return 0
+    else:
+        list_file=[]
+        for t in os.listdir(path_dire):
+            list_file.append(t)
+        return list_file
 
 if __name__== "__main__":
-    change_file_name()
+    dir_loc='/Volumes/Public/Shared Videos/TV Shows'
+    dir_loc1=0
+    lst_files= read_files(dir_loc)
+    if lst_files==0:
+        print("Please provide correct file path")
+    else:
+        print(lst_files)
+    #change_file_name()
