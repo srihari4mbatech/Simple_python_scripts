c6e826ab8dbfe13ec96570f7ef273298edd31b71
srihari4mbatech <srihari.mba@gmail.com>
2018-04-19 16:45:25 +0530

    Reading file names

--- file_name_modify.py
+++ file_name_modify.py
@@ -1,7 +1,15 @@
 # This file contains code to change file name of TV Shows as per the standard of Plex
 
+import os
+
 def change_file_name():
     pass
 
-if __name__=="__main__":
+def read_files(path_dire):
+    list_file=[]
+    for t in os.list(path_dire):
+        list_file.append(t)
+    return list_file
+
+if __name__== "__main__":
     change_file_name()
