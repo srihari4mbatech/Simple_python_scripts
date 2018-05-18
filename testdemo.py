47c63e504dadac56cd5b5051587a9569b8233008
srihari4mbatech <srihari.mba@gmail.com>
2018-05-18 14:25:03 +0530

    Adding new lines as per the test demo

--- testdemo.py
+++ testdemo.py
@@ -5,15 +5,15 @@ Accepts a dictionary containing the file owner name for each file name.
 Returns a dictionary containing a list of file names for each owner name, in any order.
 For example, for dictionary {'Input.txt': 'Randy', 'Code.py': 'Stan', 'Output.txt': 'Randy'} the group_by_owners function should return {'Randy': ['Input.txt', 'Output.txt'], 'Stan': ['Code.py']}.
 '''
+import pandas as pd
 
 
-
-class FileOwners(object):
+class FileOwners:
 
     @staticmethod
-    def group_by_owners(self,files):
-        pass
-        return None
+    def group_by_owners(files):
+        dt=pd.DataFrame(data={'keys':[*files],'users':[*files.values()]})
+        return dt.groupby('users')['keys'].agg(lambda x:list((";".join(x)).split(';'))).to_dict()
 
 files = {
     'Input.txt': 'Randy',
